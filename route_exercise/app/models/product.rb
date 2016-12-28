85e4f26acc5c80343c492268b17eff537659cccb
ccppjj1981 <macbook@macbookdeMacBook-Air.local>
2016-12-29 07:34:29 +0800

    route练习

---
+++ route_exercise/app/models/product.rb
@@ -0,0 +1,10 @@
+class Product < ApplicationRecord
+  has_many :likes
+  def liked_count
+  self.likes.size
+  end
+
+  def liked_by?(like_id)
+    !! self.likes.where(id: like_id).first
+  end
+end
